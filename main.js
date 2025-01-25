9e2abf53c7d107edb9a2b302a83ddc1445aef266
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-25 21:28:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,11 @@ const header_margin = document.createElement("p");
 header_margin.id = "header_margin";
 
 document.body.appendChild(header_margin);
+window.addEventListener("load", function() {
+  const header = document.getElementById("header");
+  const headerMargin = document.getElementById("header_margin");
+
+  // #header の高さを取得し、#header_margin に設定
+  const headerHeight = header.offsetHeight;
+  headerMargin.style.height = `${headerHeight}px`;
+});
