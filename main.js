2e1c5248b1c564ccb2d6d19590d5cbbc25468119
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-26 09:39:18 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+//文字が変更されたらiframeを変更
+const html = document.getElementById("html_code");
+const css = document.getElementById("css_code");
+const js = document.getElementById("js_code");
+const result_iframe = document.getElementById("result");
+html.addEventListener("input", resultchange);
+css.addEventListener("input", resultchange);
+js.addEventListener("input", resultchange);
+
+//変更用コード
+function resultchange() {
+  
+}
