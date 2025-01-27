aed998b79618ea71dbc72cf46a9b5ae8591a4534
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-27 21:58:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ js.addEventListener("input", resultchange);
 
 //変更用コード
 function resultchange() {
-  const html_code = document.getElementById("html_code").value;
-  const css_code = document.getElementById("css_code").value;
-  const js_code = document.getElementById("js_code").value;
+  const html_code = document.getElementById("html_code").innerText;
+  const css_code = document.getElementById("css_code").innerText;
+  const js_code = document.getElementById("js_code").innerText;
   const result_iframe = document.getElementById("result");
   result_iframe.srcdoc = 
    `<!DOCTYPE html>
